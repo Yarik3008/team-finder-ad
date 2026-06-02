f8d0c46deb3929e5f741b2deb1a62078836b9e57
Yarik3008 <Kayarek2004@yandex.ru>
2026-06-02 16:33:10 +0300

    Add documentation from Ledorub and new comment in pagination.py

--- team_finder/pagination.py
+++ team_finder/pagination.py
@@ -4,3 +4,5 @@ from django.core.paginator import Paginator
 def get_paginated_page(queryset, page_number, per_page=12):
     paginator = Paginator(queryset, per_page)
     return paginator.get_page(page_number)
+
+"""если хотим показывать последние 10 элементов, то можно использовать:"""
